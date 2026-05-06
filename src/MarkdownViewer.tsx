abaa257904400f794e01ff017ef422a1c5e5dadf
Rawiya-lou <raoiua15@gmail.com>
2026-05-06 15:28:52 +0100

    Adding the react-syntax-highliter

--- src/MarkdownViewer.tsx
+++ src/MarkdownViewer.tsx
@@ -1,17 +1,42 @@
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-interface Props{
-  content: string
+interface Props {
+  content: string;
 }
-const MarkdownViewer = ({content}: Props) => {
+const MarkdownViewer = ({ content }: Props) => {
   return (
-      // 'prose' handles all font sizes, spacing, and bullet points automatically
-    <article className='prose prose-slate lg:pose-xl dark:pros-invert max-w-none'>
+    // 'prose' handles all font sizes, spacing, and bullet points automatically
+    <article className="prose prose-slate lg:pose-xl dark:pros-invert max-w-none">
       {/* remarkPlugins enables GFM features like tables and checkboxes */}
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ inline, className, children, ...props }: any) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={atomDark}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     </article>
-  )
-}
+  );
+};
 
-export default MarkdownViewer
+export default MarkdownViewer;
